d13f56d4c35c2d01ffa790187335913f1452dfe4
Vesa Marttila <vtmarttila@gmail.com>
2010-12-16 23:11:57 +0200

    Initial commit + proof of concept.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject vertigo "0.0.1-SNAPSHOT"
+  :description "TODO: add summary of your project"
+  :dependencies [[clojure "1.2.0"]
+                 [clojure-contrib "1.2.0"]
+                 [hiccup "0.3.1"]
+                 [compojure "0.5.3"]
+                 [ring/ring-jetty-adapter "0.3.1"]])
+
---
+++ src/vertigo/core.clj
@@ -0,0 +1,46 @@
+(ns vertigo.core
+  (:require [clojure.xml :as xml]
+            [clojure.zip :as zip])
+  (:use     [clojure.contrib.zip-filter.xml]
+            [hiccup.core]
+            [compojure.core]
+            [ring.adapter.jetty]))
+
+(def url "http://finnkino.fi/xml/Schedule/?area=1002&dt=16.12.2010")
+
+(def movies (zip/xml-zip (xml/parse url)))
+
+(defn get-shows [xz] 
+  (map
+    (fn [show]
+      {:title   (xml1-> show :Title text)
+       :theatre (xml1-> show :Theatre text)
+       :time    (xml1-> show :dttmShowStart text)
+       :image   (xml1-> show :Images :EventLargeImagePortrait text)})
+    (xml-> xz :Shows :Show)))
+
+(def render-shows
+  [:ul
+    (map 
+      (fn [{title   :title
+            theatre :theatre
+            time    :time
+            image   :image}]
+        [:li title theatre time [:img {:src image}]])
+      (get-shows movies))])
+
+(defn layout [content]
+  (html [:html
+          [:head]
+          [:body
+            [:div#content
+              content]]]))
+
+(def show-page (layout render-shows))
+
+(defroutes handler
+  (GET "/" []
+    show-page))
+
+(run-jetty handler {:port 8080 :join? false})
+
