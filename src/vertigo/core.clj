2778fce4225beb3c67641359e791a83176fe703a
Vesa Marttila <vtmarttila@gmail.com>
2010-12-28 00:10:41 +0200

    Added caching and storing by date.

--- src/vertigo/core.clj
+++ src/vertigo/core.clj
@@ -1,22 +1,34 @@
 (ns views
   (:use [hiccup.core]))
 
-(defn- layout [content]
+(defn layout [content]
   (html [:html
           [:head]
           [:body
             [:div#content
               content]]]))
 
-(defn list-shows [shows]
-  [:table
-    (for [day shows]
-      [:tr
-        [:td (:time (first (:comedy day)))]
-        [:td (map :title (:comedy day))]])])
+(defn render-show [show]
+  [:div (:title show)])
 
-(defn shows [shows]
-  (layout (list-shows shows)))
+(defn show-table [shows]
+  [:table
+    [:tr
+      [:th "Date"]
+      [:th "Comedy"]
+      [:th "Action"]
+      [:th "Drama"]
+      [:th "Others"]]
+    (for [[day genres] shows]
+      [:tr
+        [:td day]
+        [:td (map render-show (:comedy genres))]
+        [:td (map render-show (:action genres))]
+        [:td (map render-show (:drama genres))]
+        [:td (map render-show (:other genres))]])])
+
+(defn calendar [shows]
+  (layout (show-table shows)))
 
 (ns vertigo.core
   (:require [clojure.xml :as xml]
@@ -30,45 +42,45 @@
 
 (defrecord Show [title
                  theatre
-                 time
                  genres
                  image])
+(def shows-db (atom {}))
 
-(defn group-by-genre [shows]
-  (group-by
-    (fn [{genres :genres}]
-      (condp some genres
-        #{"Komedia"} :comedy
-        #{"Draama"} :drama
-        #{"Toiminta"} :action
-        :other))
-    shows))
+(defn parse-shows-and-group-by-genre [xz]
+  (let [shows (for [show (xml-> xz :Shows :Show)]
+          (Show.
+            (xml1-> show :OriginalTitle text)
+            (xml1-> show :TheatreAndAuditorium text)
+            (into #{} (split (xml1-> show :Genres text) #", "))
+            (xml1-> show :Images :EventLargeImagePortrait text)))]
+    (group-by
+      (fn [{genres :genres}]
+        (condp some genres
+          #{"Komedia"} :comedy
+          #{"Draama"} :drama
+          #{"Toiminta"} :action
+          :other))
+      shows)))
 
-(defn parse-shows [xz] 
-  (for [show (xml-> xz :Shows :Show)]
-    (Show.
-      (xml1-> show :OriginalTitle text)
-      (xml1-> show :TheatreAndAuditorium text)
-      (xml1-> show :dttmShowStart text)
-      (into #{} (split (xml1-> show :Genres text) #", "))
-      (xml1-> show :Images :EventLargeImagePortrait text))))
+(defn retrieve-shows-by-date [date]
+  (let [formatted-date (unparse (formatter "dd.MM.yyyy") date)
+        cached-shows   (get @shows-db formatted-date)]
+    (if cached-shows
+      [formatted-date cached-shows]
+      (let [new-shows (parse-shows-and-group-by-genre
+                        (zip/xml-zip
+                          (xml/parse
+                            (str "http://finnkino.fi/xml/Schedule/?area=1002&dt="
+                                 formatted-date))))]
+        [formatted-date (get (swap! shows-db assoc formatted-date new-shows) formatted-date)]))))
 
-(def finnish-date-format (formatter "dd.MM.yyyy"))
-
-(def week-of-shows
-  (pmap
-    parse-shows
-    (map
-      #(zip/xml-zip (xml/parse (str "http://finnkino.fi/xml/Schedule/?area=1002&dt=" (unparse finnish-date-format %))))
-      (for [day (range 7)]
-        (.plusDays (now) day)))))
-
-(def week-of-shows-grouped-by-genre
-  (map group-by-genre (filter not-empty week-of-shows)))
+(defn retrieve-shows-for-whole-week []
+  (let [dates (take 7 (iterate #(.plusDays % 1) (now)))]
+    (map retrieve-shows-by-date dates)))
 
 (defroutes handler
   (GET "/" []
-    (views/shows week-of-shows-grouped-by-genre)))
+    (views/calendar (retrieve-shows-for-whole-week))))
 
 (run-jetty handler {:port 8080 :join? false})
 
