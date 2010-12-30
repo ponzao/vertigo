ef5cc63616c67faf35799c4d6dd702a909cf1ed0
Vesa Marttila <vtmarttila@gmail.com>
2010-12-31 00:46:26 +0200

    Removed commented out code.

--- src/vertigo/core.clj
+++ src/vertigo/core.clj
@@ -6,25 +6,6 @@
             [appengine-magic.core :as ae])
   (:use     [compojure.core]))
 
-;(defrecord Movie [id
-;                 title
-;                 theatre
-;                 genres
-;                 image])
-;
-;(defn retrieve-all-distinct-event-ids-for-stored-shows []
-;  (distinct (map
-;              :event-id
-;              (flatten
-;                (for [{shows :shows-by-genre}
-;                        (apply-on-whole-week retrieve-shows-by-date)]
-;                  (vals shows))))))
-;
-;(defn retrieve-all-events-for-stored-shows []
-;  (map
-;    retrieve-event
-;    (retrieve-all-distinct-event-ids-for-stored-shows)))
-
 (defroutes handler
   (GET "/movies" [] 
     (views/movies (common/map-on-n-days repository/retrieve-movies 4)))
